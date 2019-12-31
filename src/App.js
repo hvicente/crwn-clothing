3e263d762284d8bc023e00f967f5bee8199b04cf
Helios Vicente <helios.vicente@gmail.com>
2019-12-31 03:07:59 +0100

    Actualizar App para utilizar hooks. useState y useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -15,61 +15,56 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
-class App extends React.Component {
-  // constructor() {
-  //   super();
+const App = ({ checkUserSession, currentUser }) => {
+  // unsubscribeFromAuth = null;
 
-  //   this.state = { currentUser: null };
+  useEffect(() => {
+    checkUserSession();
+  }, [checkUserSession]);
+  // componentDidMount() {
+  //   const { checkUserSession } = this.props;
+  //   checkUserSession();
+  //   // const { setCurrentUser, collectionsArray } = this.props;
+  //   // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+  //   //   if (userAuth) {
+  //   //     const userRef = await createUserProfileDocument(userAuth);
+  //   //     userRef.onSnapshot(snapShot => {
+  //   //       setCurrentUser({
+  //   //         id: snapShot.id,
+  //   //         ...snapShot.data(),
+  //   //       });
+  //   //     });
+  //   //   }
+  //   //   setCurrentUser(userAuth); //Same as currentUser: null
+  //   //   // addColectionAndDocuments(
+  //   //   //   'collections',
+  //   //   //   collectionsArray.map(({ title, items }) => ({ title, items })),
+  //   //   // );
+  //   //   // createUserProfileDocument(user);
+  //   //   // this.setState({ currentUser: user });
+  //   // });
   // }
 
-  unsubscribeFromAuth = null;
+  // componentWillUnmount() {
+  //   this.unsubscribeFromAuth();
+  // }
 
-  componentDidMount() {
-    const { checkUserSession } = this.props;
-    checkUserSession();
-    // const { setCurrentUser, collectionsArray } = this.props;
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-    //   if (userAuth) {
-    //     const userRef = await createUserProfileDocument(userAuth);
-    //     userRef.onSnapshot(snapShot => {
-    //       setCurrentUser({
-    //         id: snapShot.id,
-    //         ...snapShot.data(),
-    //       });
-    //     });
-    //   }
-    //   setCurrentUser(userAuth); //Same as currentUser: null
-    //   // addColectionAndDocuments(
-    //   //   'collections',
-    //   //   collectionsArray.map(({ title, items }) => ({ title, items })),
-    //   // );
-    //   // createUserProfileDocument(user);
-    //   // this.setState({ currentUser: user });
-    // });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() => (this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />)}
-          />
-        </Switch>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() => (currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />)}
+        />
+      </Switch>
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,37 +7,38 @@ import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 import CollectionsOverviewContainer from '../../components/collections-overview/collection-overview.container';
 import CollectionPageContainer from '../../pages/collection/collection.container';
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
+    console.log('useEffect');
     fetchCollectionsStart();
-  }
+  }, [fetchCollectionsStart]);
+  // componentDidMount() {
+  //   const { fetchCollectionsStart } = this.props;
+  //   fetchCollectionsStart();
+  // }
 
-  render() {
-    const { match } = this.props;
-    return (
-      <div className="shop-page">
-        {/* <Route exact path={`${match.path}`} component={CollectionsOverview} />
+  return (
+    <div className="shop-page">
+      {/* <Route exact path={`${match.path}`} component={CollectionsOverview} />
         <Route path={`${match.path}/:collectionId`} component={CollectionPage} /> */}
-        <Route
-          exact
-          path={`${match.path}`}
-          // render={props => (
-          // <CollectionOverviewWithSpinner isLoading={isFetchingCollections} {...props} />
-          component={CollectionsOverviewContainer}
-          // )}
-        />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionPageContainer}
-          // render={props => (
-          //   <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />
-          // )}
-        />
-      </div>
-    );
-  }
-}
+      <Route
+        exact
+        path={`${match.path}`}
+        // render={props => (
+        // <CollectionOverviewWithSpinner isLoading={isFetchingCollections} {...props} />
+        component={CollectionsOverviewContainer}
+        // )}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+        // render={props => (
+        //   <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />
+        // )}
+      />
+    </div>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
